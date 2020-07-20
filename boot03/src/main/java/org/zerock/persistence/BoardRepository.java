a4830a694b760713c21b6a8be66483d7506400a3
Inchijeong <cde0409@naver.com>
2020-07-21 00:49:40 +0900

    페이징 처리와 정렬 전까지

---
+++ boot03/src/main/java/org/zerock/persistence/BoardRepository.java
@@ -0,0 +1,28 @@
+package org.zerock.persistence;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.zerock.domain.Board;
+
+public interface BoardRepository extends CrudRepository<Board, Long>{
+	
+	public List<Board> findBoardByTitle(String title);
+	public Collection<Board> findByWriter(String writer);
+	public Collection<Board> findByWriterLike(String writer);
+	// 작성자에 대한 like % 키워드 %
+	public Collection<Board> findByWriterContaining(String writer);
+	// 작성자에 대한 like 키워드 %
+	public Collection<Board> findByWriterStartingWith(String writer);
+	// 작성자에 대한 like % 키워드
+	public Collection<Board> findByWriterEndingWith(String writer);
+	// OR 조건의 처리
+	public Collection<Board> findByTitleContainingOrContentContaining(String title, String content);	
+	// title LIKE % ? % AND BNO > ?
+	public Collection<Board> findByTitleContainingAndBnoGreaterThan(String keyword, Long num);
+	// title LIKE % ? % AND BNO < ?
+	public Collection<Board> findByTitleContainingAndBnoLessThan(String keyword, Long num);
+	// bno > ? ORDER BY bno DESC
+	public Collection<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno); 
+}
--- boot03/src/test/java/org/zerock/Boot03ApplicationTests.java
+++ boot03/src/test/java/org/zerock/Boot03ApplicationTests.java
@@ -1,11 +1,101 @@
 package org.zerock;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Collection;
+import java.util.List;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.Board;
+import org.zerock.persistence.BoardRepository;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class Boot03ApplicationTests {
 
+	@Autowired
+	private BoardRepository repo;
+	
+	@Test
+	public void testInsert200() {
+		for(int i = 0; i <= 200; i++) {
+			
+			Board board = new Board();
+			board.setTitle("제목.." + i);
+			board.setContent("내용 ...." + i + " 채우기");
+			board.setWriter("user0"+(i % 10));
+			repo.save(board);
+		}
+	}
+	
+	@Test
+	public void testByTitleOld() {
+		
+		// Before Java 8
+		List<Board> list = repo.findBoardByTitle("제목..117");
+		
+		for(int i = 0, len = list.size(); i < len; i++) {
+			System.out.println(list.get(i));
+		}		
+	}
+	
+	@Test
+	public void testByTitleNew() {
+		
+		repo.findBoardByTitle("제목..117")
+			.forEach(board -> System.out.println(board));
+	}
+
+	@Test
+	public void testByWriter() {
+		
+		Collection<Board> results = repo.findByWriter("user00");
+		
+		results.forEach(board -> System.out.println(board));
+	}
+	
+	@Test
+	public void testByWriterContaining() {
+		
+		Collection<Board> results = repo.findByWriterContaining("05");
+		
+		results.forEach(board -> System.out.println(board));
+	}
+	
+	@Test
+	public void testByWriterEndingWith() {
+		
+		Collection<Board> results = repo.findByWriterEndingWith("05");
+		
+		results.forEach(board -> System.out.println(board));
+	}
+	
+	@Test
+	public void testByTitleContainingOrContentContaining() {
+		
+		Collection<Board> results = repo.findByTitleContainingOrContentContaining("5", "05");
+		
+		results.forEach(board -> System.out.println(board));
+	}
+	
+	@Test
+	public void testByTitleContainingAndBnoLessThan() {
+		
+		Collection<Board> results = repo.findByTitleContainingAndBnoLessThan("1", 50L);
+		
+		results.forEach(board -> System.out.println(board));
+	}
+	
+	@Test
+	public void testByBnoGreaterThanOrderByBnoDesc() {
+		
+		Collection<Board> results = repo.findByBnoGreaterThanOrderByBnoDesc(50L);
+		
+		results.forEach(board -> System.out.println(board));
+	}
+	
 	@Test
 	void contextLoads() {
 	}
