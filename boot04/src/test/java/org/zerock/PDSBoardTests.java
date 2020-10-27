58c4018c8edf53256e708eeb510d688882eafbd5
Inchijeong <cde0409@naver.com>
2020-10-27 23:55:46 +0900

    [P177] 첨부 파일 삭제 전까지

--- boot04/src/test/java/org/zerock/PDSBoardTests.java
+++ boot04/src/test/java/org/zerock/PDSBoardTests.java
@@ -1,11 +1,16 @@
 package org.zerock;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.zerock.domain.PDSBoard;
 import org.zerock.domain.PDSFile;
@@ -16,6 +21,7 @@ import lombok.extern.java.Log;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Log
+@Commit
 public class PDSBoardTests {
 
 	@Autowired
@@ -39,4 +45,48 @@ public class PDSBoardTests {
 
 		repo.save(pds);
 	}
+	
+	@Transactional
+	@Test
+	public void testUpdateFileName1() {
+		
+		Long fno = 1L;
+		String newName = "updatedFile1.doc";
+		
+		int count = repo.updatePDSFile(fno, newName);
+		// @Log 설정된 이후 사용 가능
+		log.info("update count: " + count);		
+	}
+	
+	@Transactional
+	@Test
+	public void testUpdateFileName2() {
+		
+		String newName = "updatedFile2.doc";
+		// 반드시 번호가 존재하는 확인할 것
+		Optional<PDSBoard> result = repo.findById(2L);
+		
+		result.ifPresent(pds -> {
+			
+			log.info("데이터가 존재하므로 update 시도");
+			
+			PDSFile target = new PDSFile();
+			
+			target.setFno(2L);
+			target.setPdsfile(newName);
+			
+			int idx = pds.getFiles().indexOf(target);
+			
+			if(idx > -1) {
+				
+				List<PDSFile> list = pds.getFiles();
+				list.remove(idx);
+				list.add(target);
+				pds.setFiles(list);				
+			}
+			
+			repo.save(pds);			
+		});
+	}
+	
 }
