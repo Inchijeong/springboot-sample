c4746f5f67dcfb0d2f210dc85f3c41eb8a28048b
Inchijeong <cde0409@naver.com>
2021-02-07 18:09:27 +0900

    게시물 등록

--- boot06/src/main/java/org/zerock/controller/WebBoardController.java
+++ boot06/src/main/java/org/zerock/controller/WebBoardController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.WebBoard;
 import org.zerock.persistence.WebBoardRepository;
 import org.zerock.vo.PageMaker;
@@ -51,4 +53,22 @@ public class WebBoardController {
 		model.addAttribute("result", new PageMaker(result));
 	}
 
+	@GetMapping("register")
+	public void registerGET(@ModelAttribute("vo") WebBoard vo) {
+		
+		log.info("register get");
+	}
+	
+	@PostMapping("register")
+	public String registerPOST(@ModelAttribute("vo") WebBoard vo, RedirectAttributes rttr) {
+		
+		log.info("register post");
+		log.info("" + vo);
+		
+		repo.save(vo);
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "redirect:/boards/list";
+	}
+	
 }
