648d616c4c950517098a03d5a0ab0e81cbc118f0
Inchijeong <cde0409@naver.com>
2021-03-05 18:46:59 +0900

    board 삭제

--- board/src/main/java/com/ex/board/domain/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ex.board.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ex.board.domain.entity.BoardEntity;
-
-public interface BoardRepository extends JpaRepository<BoardEntity, Long>{
-
-}
